8ca09f587c7695e300859b17e89af2e12c70544b
Lars Wikman <lars@underjord.io>
2026-01-29 09:38:59 +0100

    🔥 initial commit 🔥

---
+++ lib/nerves_mcp.ex
@@ -0,0 +1,18 @@
+defmodule NervesMCP do
+  @moduledoc """
+  Documentation for `NervesMCP`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesMCP.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_mcp_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesMCPTest do
+  use ExUnit.Case
+  doctest NervesMCP
+
+  test "greets the world" do
+    assert NervesMCP.hello() == :world
+  end
+end
